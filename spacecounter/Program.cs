47cbd550368e0aa652d89d8d3490141ba2ffa084
Rahei <Rahei@RAHEIMSPC>
2024-11-18 14:00:42 -0500

    Add project files.

---
+++ spacecounter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace spacecounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter a sentence");
+            string sentence = Console.ReadLine();
+            
+            char spaceChar = ' ';
+            
+            int spaceCount =0;
+            foreach (char c in sentence)
+            {
+                if (spaceChar == c)
+                spaceCount++;
+                
+            }
+            Console.WriteLine($"there are {spaceCount} space(s) in your sentence \n{sentence}");
+            
+
+            
+        }
+    }
+}
